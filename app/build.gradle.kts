7600ada1bc25b9942df952440dc9f9bf229d008c
seonhakji <jshag1990@gmail.com>
2025-06-19 00:07:19 +0900

    [Feat] 광고 permission 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.dodamsoft.fivembti"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "애드몹광고추가"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
